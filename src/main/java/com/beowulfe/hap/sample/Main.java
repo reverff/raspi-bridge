88a4cc3ed70dd5c254c9b864eee9b95ff36981f6
mgavlovy <maksym.gavlovych@lifecell.com.ua>
2016-10-22 21:46:37 +0300

    add interface for motor

--- src/main/java/com/beowulfe/hap/sample/Main.java
+++ src/main/java/com/beowulfe/hap/sample/Main.java
@@ -6,13 +6,13 @@ import org.apache.log4j.Logger;
 
 public class Main {
 	private static final Logger log = Logger.getLogger(Main.class);
-	private static final int PORT = 9123;
+	private static final int PORT = 8080;
 	
 	public static void main(String[] args) {
         log.info("Starting application");
 		try {
                 HomekitServer homeKit = new HomekitServer(PORT);
-                HomekitRoot bridge = homeKit.createBridge(new RaspiBridgeAuthInfo(), "Test Bridge", "TestBridge, Inc.", "G6", "111abe234");
+                HomekitRoot bridge = homeKit.createBridge(new RaspiBridgeAuthInfo(), "Test Bridge pc", "TestBridge, Inc.", "G6", "111abe234");
                 bridge.addAccessory(new WindowCoveringComponent());
                 bridge.start();
 		} catch (Exception e) {
