0e6650b5ad6db900041ed546251ed6a1791f5405
mgavlovy <maksym.gavlovych@lifecell.com.ua>
2016-10-22 22:08:23 +0300

    add interface for motor

--- src/main/java/com/beowulfe/hap/sample/Main.java
+++ src/main/java/com/beowulfe/hap/sample/Main.java
@@ -4,14 +4,20 @@ import com.beowulfe.hap.HomekitRoot;
 import com.beowulfe.hap.HomekitServer;
 import org.apache.log4j.Logger;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Main {
 	private static final Logger log = Logger.getLogger(Main.class);
-	private static final int PORT = 8080;
+    private static final int PORT = 8080;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
         log.info("Starting application");
+        InetAddress address = Inet4Address.getLocalHost();
+        log.info("Current environment: " + address.getHostName() + " - " + address.getHostAddress() + " | " + address.getClass().getSimpleName());
 		try {
-                HomekitServer homeKit = new HomekitServer(PORT);
+                HomekitServer homeKit = new HomekitServer(address, PORT);
                 HomekitRoot bridge = homeKit.createBridge(new RaspiBridgeAuthInfo(), "Test Bridge pc", "TestBridge, Inc.", "G6", "111abe234");
                 bridge.addAccessory(new WindowCoveringComponent());
                 bridge.start();
